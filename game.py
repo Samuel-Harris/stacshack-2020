b4df7ed9b8f9b71fd932a60b78a82a68a056730a
Jonathan Yim <canpake@users.noreply.github.com>
2020-03-07 17:40:14 +0000

    added background image; infinite background scrolling

--- game.py
+++ game.py
@@ -24,9 +24,11 @@ def main():
     pg.mouse.set_visible(0)
 
     # Create The Background
-    background = pg.Surface(screen.get_size())
-    background = background.convert()
-    background.fill((250, 250, 250))
+    # background = pg.Surface(screen.get_size())
+    # background = background.convert()
+    # background.fill((250, 250, 250))
+    bg = pg.image.load("art/background/background.png")
+    bg_offset = 0
 
     # Put Text On The Background, Centered
     # if pg.font:
@@ -36,7 +38,7 @@ def main():
     #     background.blit(text, textpos)
 
     # Display The Background
-    screen.blit(background, (0, 0))
+    screen.blit(bg, (0, 0))
     # pg.display.flip()
 
     # Prepare Game Objects
@@ -54,8 +56,13 @@ def main():
     # Main Loop
     going = True
     while going:
-        # draw the background
-        screen.blit(background, (0, 0))
+        # draw two backgrounds, slowly moving down
+        screen.blit(bg, (0, bg_offset))
+        screen.blit(bg, (0, bg_offset - screen_height))
+        if bg_offset < screen_height:
+            bg_offset += 1
+        else:   # once offset goes off the screen, reset it
+            bg_offset = 0
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
