284b0ae68409f1e3f7de8666b57e24e84b185a86
Anson <git@anson-cheung.com>
2020-03-07 12:14:20 +0000

    Add more files

--- game.py
+++ game.py
@@ -1,42 +1,7 @@
 # Import Modules
-import os
 import pygame as pg
-from pygame.compat import geterror
 
-main_dir = os.path.split(os.path.abspath(__file__))[0]
-data_dir = os.path.join(main_dir, "assets")
-
-
-# functions to create our resources
-def load_image(name, colorkey=None):
-    fullname = os.path.join(data_dir, name)
-    try:
-        image = pg.image.load(fullname)
-    except pg.error:
-        print("Cannot load image:", fullname)
-        raise SystemExit(str(geterror()))
-    image = image.convert()
-    if colorkey is not None:
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey, pg.RLEACCEL)
-    return image, image.get_rect()
-
-
-def load_sound(name):
-    class NoneSound:
-        def play(self):
-            pass
-
-    if not pg.mixer or not pg.mixer.get_init():
-        return NoneSound()
-    fullname = os.path.join(data_dir, name)
-    try:
-        sound = pg.mixer.Sound(fullname)
-    except pg.error:
-        print("Cannot load sound: %s" % fullname)
-        raise SystemExit(str(geterror()))
-    return sound
+from player import Player
 
 
 def main():
@@ -67,12 +32,8 @@ def main():
 
     # Prepare Game Objects
     clock = pg.time.Clock()
-    whiff_sound = load_sound("whiff.wav")
-    punch_sound = load_sound("punch.wav")
-    # chimp = Chimp()
-    # fist = Fist()
-    # allsprites = pg.sprite.RenderPlain((fist, chimp))
-    allsprites = pg.sprite.RenderPlain()
+    player = Player()
+    allsprites = pg.sprite.RenderPlain(player)
 
     # Main Loop
     going = True
